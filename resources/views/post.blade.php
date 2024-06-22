848429da2d84815fc8939d5ab338e90ec756fe36
Michinded <michinded@gmail.com>
2024-06-22 02:49:32 -0600

    Update post.blade.php

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,13 +4,13 @@
 
 
 @section('content')
-    <div class="bg-white dark:bg-gray-800 dark:text-white p-4 shadow rounded">
+    <div class="bg-white dark:bg-gray-800 dark:text-white max-w-3xl mx-auto px-6">
         <h2 class="text-lg font-bold">{{$post->title}}</h2>
         <!-- autor del post -->
         <p class="text-sm text-gray-500">Por {{$post->user->name}}</p>
         <!-- fecha de publicación -->
         <p class="text-sm text-gray-500">{{$post->created_at->format('d/m/Y')}}</p>
         <!-- contenido del post -->
-        <p class="mt-2">{{$post->body}}</p>
+        <p class="leading-loose text-lg">{{$post->body}}</p>
     </div>
 @endsection
