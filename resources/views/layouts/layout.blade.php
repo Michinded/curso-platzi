63a4840f97b74c1d63b9dd18495878e3e7663996
Michinded <michinded@gmail.com>
2024-06-22 00:00:11 -0600

    Update layout.blade.php
    Usar tailwind desde Breeze (Vite)

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @yield('head')
 </head>
 <body>
