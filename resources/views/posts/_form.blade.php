3c5ff75993ba1ce11b15b15ac97ca9e2f0bd6345
Michinded <michinded@gmail.com>
2024-06-22 01:46:35 -0600

    Update _form.blade.php

--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -11,15 +11,19 @@
     <!-- Title -->
     <x-input-label for="title" :value="__('Título')" />
     <x-text-input id="title" name="title" type="text" class="mt-1 block w-full" :value="old('title', $post->title)" required autofocus autocomplete="title" />
-    @error('title')
+    <span class="text-red-500">
+        @error('title')
         <p class="text-red-500 text-xs italic mt-4">{{ $message }}</p>
-    @enderror
+        @enderror
+    </span>
     <!-- Body -->
     <x-input-label for="body" :value="__('Contenido')" />
     <textarea name="body" id="body" cols="30" rows="10" class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">{{old('body', $post->body)}}</textarea>
-    @error('body')
+    <span class="text-red-500">
+        @error('body')
         <p class="text-red-500 text-xs italic mt-4">{{ $message }}</p>
-    @enderror
+        @enderror
+    </span>
 </div>
 
 <div>
