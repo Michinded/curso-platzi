7d331c00f340d8416c132bae1a151b83d5c99d22
Michinded <michinded@gmail.com>
2024-06-21 23:30:30 -0600

    Update PageController.php
    Actualizar funciones

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -13,11 +13,26 @@ class PageController extends Controller
 
     public function blog()
     {
-        return view('blog');
+
+        /*
+            Tipos de consultas
+            $posts = Post::all(); //Obtiene todos los registros
+            $posts = Post::where('status', 2)->get(); //Obtiene los registros que cumplan con la condición
+            get() //Obtiene los registros
+            first() //Obtiene el primer registro
+            find() //Obtiene un registro por su id
+            latest() //Obtiene los registros ordenados por la fecha de creación
+            oldest() //Obtiene los registros ordenados por la fecha de creación
+            orderBy('column', 'asc') //Ordena los registros por la columna indicada
+        */
+
+        //Obtenemos los posts de la base de datos
+        $posts = Post::latest()->paginate(15);
+        return view('blog', compact('posts'));
     }
 
-    public function post()
+    public function post(Post $post)
     {
-        return view('post');
+        return view('post', compact('post'));
     }
 }
