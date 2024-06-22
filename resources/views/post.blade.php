eda2811431db3bffe9daba3d29b589ded684994b
Michinded <michinded@gmail.com>
2024-06-21 23:30:03 -0600

    Update post.blade.php
    Mostrar información del post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,4 +6,11 @@
 @section('content')
     <h1>Post</h1>
     <p>Este es el contenido de la página de post.</p>
+    <div class="bg-white p-4 shadow rounded">
+        <h2 class="text-lg font-bold">{{$post->title}}</h2>
+        <!-- fecha de publicación -->
+        <p class="text-sm text-gray-500">{{$post->created_at->format('d/m/Y')}}</p>
+        <!-- contenido del post -->
+        <p class="mt-2">{{$post->body}}</p>
+    </div>
 @endsection
