434d8f366053779fa8a2091d8842f94b7229693a
Michinded <michinded@gmail.com>
2024-06-23 04:27:31 -0600

    Se usan los alias para las versiones
    Agregar nuevas rutas

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\PostController;
+use App\Http\Controllers\Api\V1\PostController as PostCV1;
+use App\Http\Controllers\Api\V2\PostController as PostCV2;
 
 Route::prefix('v1')->group(function() {
-    Route::apiResource('posts', PostController::class)
+    Route::apiResource('posts', PostCV1::class)
+    ->only(['index', 'show', 'destroy']);
+});
+
+Route::prefix('v2')->group(function() {
+    Route::apiResource('posts', PostCV2::class)
     ->only(['index', 'show', 'destroy']);
 });
