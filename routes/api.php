99155d5060d427309abd1c32278dd6e1d9d493ba
Michinded <michinded@gmail.com>
2024-06-23 04:03:19 -0600

    Update api.php
    agregar ruta index

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\PostController;
 
 Route::prefix('v1')->group(function() {
-    Route::apiResource('posts', PostController::class)->only('show');
+    Route::apiResource('posts', PostController::class)
+    ->only(['index', 'show']);
 });
