2492003724542d1040ff68c228e16d821ffb9eb6
Michinded <michinded@gmail.com>
2024-06-23 03:50:01 -0600

    Update api.php
    crear el recurso de rutas de api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\PostController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function() {
+    Route::apiResource('posts', PostController::class)->only('show');
+});
