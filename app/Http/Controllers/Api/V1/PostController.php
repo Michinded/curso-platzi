32b2afe80d9f5e70992f86924c591688d1c991e2
Michinded <michinded@gmail.com>
2024-06-23 04:10:15 -0600

    Metodo destroy

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -49,5 +49,12 @@ class PostController extends Controller
     public function destroy(Post $post)
     {
         //
+        $post->delete();
+
+        return response()->json(
+            [
+                'message' => 'Post deleted successfully'
+            ]
+        );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,5 @@ use App\Http\Controllers\Api\V1\PostController;
 
 Route::prefix('v1')->group(function() {
     Route::apiResource('posts', PostController::class)
-    ->only(['index', 'show']);
+    ->only(['index', 'show', 'destroy']);
 });
